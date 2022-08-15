0a77a0b402898f087fb941340b8061250f3bf9fc
hamzahnur2004 <111177818+hamzahnur2004@users.noreply.github.com>
2022-08-15 20:28:55 +0700

    Add files via upload

---
+++ Coding-PGT/kemampuan-dasar-1/Latihan/powershellscripts.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host "Hello, world!"
+echo "Hello, world!"
+Get-WmiObject -Class win32_operatingsystem -ComputerName localhost
+Select-Object -Property CSName,@{n="Last Booted";
+e={[management.managementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
